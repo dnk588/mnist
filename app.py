b1adf9910faa0702cbc260b95cd5a4e28213cd58
Daniyar Seidaliyev <dnk588@gmail.com>
2023-11-10 17:45:20 +0600

    bunch of files

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from PIL import Image
+from segment import process
+
+st.title('Mnist demo')
+
+image_file = st.file_uploader('Load an image', type=['png', 'jpg'])  # Добавление загрузчика файлов
+
+if not image_file is None:                       # Выполнение блока, если загружено изображение
+    image = Image.open(image_file)               # Открытие изображения
+    results = process(image_file)                # Обработка изображения с помощью функции, реализованной в другом файле
+    st.write(results)
