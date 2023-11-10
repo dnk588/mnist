3fcf767d28b8a1b74c3680065e8789f197c66ee5
Daniyar Seidaliyev <dnk588@gmail.com>
2023-11-10 18:07:33 +0600

    working with local

--- segment.py
+++ segment.py
@@ -2,12 +2,13 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-MODEL_NAME =   'model_fmr_all.h5'
+MODEL_NAME = 'model_fmr_all.h5'
 model = load_model(MODEL_NAME)
+
 
 def process(image_file):
     picture = Image.open(image_file)
-    new_pic = image.img_to_array(image.load_img(picture, target_size=(28, 28), color_mode='grayscale'))
-    new_pic = new_pic.reshape(-1, 784)
+    new_pic = image.img_to_array(image.load_img('0.png', target_size=(28, 28), color_mode='grayscale'))
+    new_pic = new_pic.reshape(-1, 28, 28)
     prediction = model.predict(new_pic)
     return prediction
