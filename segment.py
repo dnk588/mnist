824178340272b0fd332b518e2e593e9746bc2f7f
Daniyar Seidaliyev <dnk588@gmail.com>
2023-11-10 18:46:29 +0600

    working

--- segment.py
+++ segment.py
@@ -7,8 +7,9 @@ model = load_model(MODEL_NAME)
 
 
 def process(image_file):
-    picture = Image.open(image_file)
-    new_pic = image.img_to_array(image.load_img('0.png', target_size=(28, 28), color_mode='grayscale'))
+    picture = Image.open(image_file).convert('L').resize((28, 28))
+    new_pic = image.img_to_array(picture)
     new_pic = new_pic.reshape(-1, 28, 28)
     prediction = model.predict(new_pic)
+
     return prediction
