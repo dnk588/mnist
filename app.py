fe62760a1de18247f91cd9d0b55e4b0b3971359b
Daniyar Seidaliyev <dnk588@gmail.com>
2023-11-10 18:53:51 +0600

    small fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from segment import process
 
 st.title('Mnist demo')
 
-image_file = st.file_uploader('Load an image', type=['png', 'jpg'])  # Добавление загрузчика файлов
+image_file = st.file_uploader('Load an image with number', type=['png', 'jpg'])  # Добавление загрузчика файлов
 
 if not image_file is None:                       # Выполнение блока, если загружено изображение
     image = Image.open(image_file)               # Открытие изображения
