e8787a7f26ae157f87cc859162fd05c99d665cbc
Daniyar Seidaliyev <dnk588@gmail.com>
2023-11-21 19:50:09 +0600

    текст добавлен

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ image_file = st.file_uploader('Load an image with number', type=['png', 'jpg']) 
 if not image_file is None:                       # Выполнение блока, если загружено изображение
     image = Image.open(image_file)               # Открытие изображения
     results = process(image_file)                # Обработка изображения с помощью функции, реализованной в другом файле
+    st.write('Таблица вероятностей цифры загруженной картинки:')
     st.write(results)
